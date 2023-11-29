d428b5c5184a582c93224de64ce0e6ed736da707
JoeKrister <147590985+JoeKrister@users.noreply.github.com>
2023-11-29 09:18:43 +0100

    fin hanoi

--- LPF/tour_de_hanoi.py
+++ LPF/tour_de_hanoi.py
@@ -15,10 +15,7 @@ class Pile:
         self._taille = 0
         
     def est_vide(self):
-        if self.p is None:
-            return True
-        else:
-            return False
+        return self.p is None
     
     def empiler(self, n:int):
         self._taille += 1
@@ -33,59 +30,70 @@ class Pile:
         return v
           
     def consulter(self):
-        if p.est_vide() :
+        if self.est_vide() :
             return float("inf")
-        else:
-            return str(self.p.valeur)
+        return self.p.valeur
         
     def __str__(self):
-        if self.p is None :
+        if self.est_vide() :
             return ""
-        else :
-            return f"{self.p.valeur} <- {self.p.suivant}"
+        return str(self.p)
     
 
 class HanoiGame:
     def __init__(self, n:int):
-        p0 = Pile()
-        p1 = Pile()
-        p2 = Pile()
-        self.n = 0
-        self.piles = [p0, p1, p2]
+        self.n = n
+        self.piles = [Pile(), Pile(), Pile()]
         self.petit_a_bouger = False
-        self.position_petit = self.piles[0]
+        self.position_petit = 0
         for i in range(n):
             self.piles[0].empiler(n-i)
         
     def show(self): 
-        print(f'''
-                D: {h.piles[0]}
-                I: {h.piles[1]}
-                F: {h.piles[2]}''')
+        print( f"""
+D : {self.piles[0]}
+I : {self.piles[1]}
+A : {self.piles[2]}
+        """)
     
     def next_move(self):
-        if self.petit_a_bouger == False:
-            positions_possibles = [0, 1, 2].remove([self.position_petit])
-            h.piles[positions_possibles] = self.empiler(n)
-        else :
-            if self.position_petit == 2:
-                self.depiler()
-                h.piles[1] = self.empiler(self.position_petit)
-            elif self.position_petit == 1:
-                self.depiler()
-                h.piles[0] = self.empiler(self.position_petit)
-            elif self.position_petit == 0:
-                self.depiler()
-                h.piles[2] = self.empiler(self.position_petit)
-            
-            
-            
-            
-    def solve(self, verbose = True) -> int:                
-        if verbose == True :
-            return self.p.show
+        if not(self.petit_a_bouger):
+            next_position = (self.position_petit-1)%3
+            self.piles[next_position].empiler(self.piles[self.position_petit].depiler())
+            self.position_petit = next_position
+            self.petit_a_bouger = True
         else:
-            return None
+            tours = [0, 1, 2]
+            tours.remove(self.position_petit)
+            tourA, tourB = tours
+            if self.piles[tourA].consulter() < self.piles[tourB].consulter():
+                self.piles[tourB].empiler(self.piles[tourA].depiler())
+            elif self.piles[tourB].consulter() < self.piles[tourA].consulter():
+                self.piles[tourA].empiler(self.piles[tourB].depiler())
+            self.petit_a_bouger = False
+                     
+    def solve(self, verbose = True) -> int:
+        nb_etapes = 0
+        while not(self.piles[0].est_vide() and self.piles[1].est_vide()):
+            self.next_move()
+            nb_etapes += 1
+            if verbose == True :
+                self.show()
+                print(f"""étape numéro: {nb_etapes}""")
+        return nb_etapes
 
-h = HanoiGame(3)
-p = Pile()
+    def solve_rec(self) :
+        def solve_r(n, d, i, a, nb_move) :
+            if n == 1 :
+                self.piles[a].empiler(self.piles[d].depiler())
+                return 1
+            else :
+                nb_move += solve_r(n-1, d, a, i, 0)
+                self.piles[a].empiler(self.piles[d].depiler())
+                nb_move +=1
+                nb_move += solve_r(n-1, i, d, a, 0)
+                return nb_move
+        return solve_r(self.n, 0, 1, 2, 0)
+
+
+
