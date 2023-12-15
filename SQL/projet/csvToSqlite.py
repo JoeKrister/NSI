c5d465d89466b958a77e6c435df81a7ac5909d5d
JoeKrister <147590985+JoeKrister@users.noreply.github.com>
2023-12-15 23:11:20 +0100

    création db

--- SQL/projet/csvToSqlite.py
+++ SQL/projet/csvToSqlite.py
@@ -1,47 +1,37 @@
 import csv, sqlite3
 
-con = sqlite3.connect('pokemon.db')
+con = sqlite3.connect('collection_piece.db')
 
-with open('pokemon.csv', newline='', encoding='utf8') as csvfile:
+with open('collectio_piece.csv', newline='', encoding='utf8') as csvfile:
     reader = csv.reader(csvfile)
     curseur=con.cursor()
-    curseur.execute("""DROP TABLE IF EXISTS Pokemon""")
+    curseur.execute("""DROP TABLE IF EXISTS piece""")
     curseur.execute("""
-    CREATE TABLE IF NOT EXISTS Pokemon (
+    CREATE TABLE IF NOT EXISTS collection_piece (
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name TEXT,
-    imageName TEXT,
-    type1 TEXT,
-    type2 TEXT,
-    total INTEGER,
-    hp INTEGER,
-    attack INTEGER,
-    defense INTEGER,
-    sp_attack INTEGER,
-    sp_defense INTEGER,
-    speed INTEGER,
-    generation INTEGER,
-    legendary BOOLEAN)
+    nom TEXT,
+    nb_exemple SMALLINT,
+    emetteur VARCHAR(90),
+    type VARCHAR(90),
+    date SMALLINT,
+    devise VARCHAR(90),
+    composition VARCHAR(20),
+    poids REAL,
+    diametre REAL,
+    epaisseur REAL,
+    forme VARCHAR(20),
+    demonetisee BOOLEAN,
+    indice_rarete SMALLINT,
+    avers TEXT,
+    envers TEXT)
     """)
     for line in list(reader)[1:] :
-        if " "in line[1] :
-            splited = line[1].split(" ")
-            print(splited)
-            for t in splited[1: ] :
-                
-                if len(t)>1 and t in splited[0] :
-                    newl = splited[0].split(t)[-1]
-                    name = newl+" "+" ".join(splited[1:])
-                    imgname = "-".join(name.split(" "))
-        else :
-                name= line[1]
-                imgname= line[1]
         req = f"""
-    INSERT INTO Pokemon (name, imagename, type1, type2, total, hp,
-    attack, defense,sp_attack, sp_defense, speed, generation, legendary
-    ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);
+    INSERT INTO collection_piece (id, nom, nb_exemple, emetteur, type, date,
+    devise, composition,poids, diametre, epaisseur, forme, demonetisee, indice_rarete, avers, envers
+    ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);
         """
-        curseur.execute(req,[name.lower(),imgname.lower(), *line[2:]] )
+        curseur.execute(req,[*line] )
 con.commit()
 con.close()
         
