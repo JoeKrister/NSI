e6d50715830f30899d48b38334e72c7347570a56
JoeKrister <147590985+JoeKrister@users.noreply.github.com>
2023-12-20 09:53:49 +0100

    avancée html+javascript

--- SQL/projet/csvToSqlite.py
+++ SQL/projet/csvToSqlite.py
@@ -1,36 +1,29 @@
 import csv, sqlite3
 
-con = sqlite3.connect('pokemon.db')
+con = sqlite3.connect('collection.db')
 liste_types = set()
-pokemons=[]
-with open('pokemon.csv', newline='', encoding='utf8') as csvfile:
+collections=[]
+with open('collection_piece_29.csv', newline='', encoding='utf8') as csvfile:
     reader = csv.reader(csvfile)
     curseur=con.cursor()
-    curseur.execute("""DROP TABLE IF EXISTS Pokemon""")
-    curseur.execute("""DROP TABLE IF EXISTS Type""")
-    curseur.execute("""
-    CREATE TABLE IF NOT EXISTS Type (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,nom_type VARCHAR(30))
-    """)
-    
-    curseur.execute("""
-    CREATE TABLE IF NOT EXISTS Pokemon (
-    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name TEXT,
-    imageName TEXT,
-    form VARCHAR(40),
-    type1 INT,
-    type2 INT,
-    total INTEGER,
-    hp INTEGER,
-    attack INTEGER,
-    defense INTEGER,
-    sp_attack INTEGER,
-    sp_defense INTEGER,
-    speed INTEGER,
-    generation INTEGER,
-    FOREIGN KEY (type1) REFERENCES Type(id),
-    FOREIGN KEY (type2) REFERENCES Type(id)
-    )
+    curseur.execute("""DROP TABLE IF EXISTS collection""")
+    curseur.execute("""CREATE TABLE IF NOT EXISTS collection
+    (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    nom VARCHAR(40),
+    nb_exemplaire SMALLINT,
+    emetteur VARCHAR(40),
+    type VARCHAR(40),
+    date SMALLINT,
+    devise VARCHAR(40),
+    composition VARCHAR(20),
+    poids REAL,
+    diametre REAL,
+    epaisseur REAL,
+    forme VARCHAR(20),
+    demonetise CHAR(3),
+    indice_rarete SMALLINT,
+    avers TEXT,
+    envers TEXT;)
     """)
     for line in list(reader)[1:] :
         if " "in line[1] :
@@ -51,22 +44,22 @@ with open('pokemon.csv', newline='', encoding='utf8') as csvfile:
         if type2 != "" :
             liste_types=liste_types | set([type2])
         
-        pokemons.append([name.lower(),imgname.lower(), line[2], type1, type2, *line[5:]])
+        collections.append([name.lower(),imgname.lower(), line[2], type1, type2, *line[5:]])
 con.commit()
 con.close()
-con = sqlite3.connect('pokemon.db')
+con = sqlite3.connect('collection.db')
 curseur=con.cursor()
 for typ in liste_types :
     req = f"""INSERT INTO Type(nom_type) VALUES (?);"""
     curseur.execute(req, [typ])
 con.commit()
 con.close()
-con = sqlite3.connect('pokemon.db')
+con = sqlite3.connect('collection.db')
 curseur=con.cursor()
 
 
 
-for pok in pokemons :
+for pok in collections :
     type1=pok[3]
     type2=pok[4]
     if type1 !="" :
@@ -85,12 +78,15 @@ for pok in pokemons :
         id_type2 = None
         
     
-    req = f"""
-    INSERT INTO Pokemon (name, imagename, form, type1, type2, total, hp,
-    attack, defense,sp_attack, sp_defense, speed, generation
-    ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);
-        """
-    curseur.execute(req,[pok[0], pok[1], pok[2], id_type1, id_type2, *pok[5:]] )
+    con.commit()
+con.close()
+
+   
+req = f""" INSERT INTO collection (id, nom, nb_exemplaire, emetteur, type, date, devise, composition, poids, diametre, epaisseur, forme, demonetise, indice_rarete, avers, envers    ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"""
+curseur.execute(req,[pok[0], pok[1], pok[2], id_type1, id_type2, *pok[5:]] )
+con.commit()
+con.close()
+curseur.execute(req,[pok[0], pok[1], pok[2], id_type1, id_type2, *pok[5:]] )
 con.commit()
 con.close()
         
